a6e954dfcccc0d2313898bf2f0dddd8b4426c567
nodestarQ <node@perfectqstudios.com>
2025-04-24 21:30:23 +0200

    feat: added obsidion wallet implementation, still need to access data tho

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
+    import WalletTest from './lib/WalletTest.svelte';
   import placeHolderLogo from '/logo.svg'
 </script>
 
 <main class="flex items-center justify-center h-screen">
-  <div class="">
+  <div class="text-center grid gap-2">
     <a href="https://github.com/JossDuff/warp-toad#" target="_blank" rel="noreferrer" class="justify-center flex">
       <img src={placeHolderLogo} class="logo" alt="WarpToad Logo" />
     </a>
     <h1 class="text-base-content">warptoad svelte boilerplate</h1>
+    <WalletTest/>
   </div>
 </main>
 
---
+++ frontend/src/lib/WalletTest.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { AztecWalletSdk, obsidion } from "@nemi-fi/wallet-sdk"
+
+  let account: any = null; 
+
+  const NODE_URL = "https://registry.obsidion.xyz/node" // testnet
+
+  const WALLET_URL = "https://app.obsidion.xyz"
+
+  // This should be instantiated outside of any js classes / react components
+  const sdk = new AztecWalletSdk({
+    aztecNode: NODE_URL,
+    connectors: [obsidion({ walletUrl: WALLET_URL })],
+  })
+
+
+
+
+  const connectWallet = async () => {
+    try {
+      account = await sdk.connect("obsidion")
+      console.log("WALLET CONNECTED")
+      console.log(account)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  let count: number = $state(0);
+  const increment = () => {
+    count += 1;
+  };
+</script>
+
+<button class="btn btn-primary" onclick={connectWallet}>
+  WalletTestButton: {account}
+</button>
