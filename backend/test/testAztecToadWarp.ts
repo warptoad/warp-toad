5c94ff65972ac82c9812ef0b78eaa4eca4bb127b
jimjimvalkema <jjvalkema@protonmail.com>
2025-04-25 16:00:16 +0200

    cleaned up L1 mint and burn test

--- backend/test/testAztecToadWarp.ts
+++ backend/test/testAztecToadWarp.ts
@@ -39,7 +39,7 @@ async function connectPXE() {
 
 
 
-describe("L1WarpToad", function () {
+describe("AztecWarpToad", function () {
     // We define a fixture to reuse the same setup in every test.
     // We use loadFixture to run this setup once, snapshot that state,
     // and reset Hardhat Network to that snapshot in every test.
