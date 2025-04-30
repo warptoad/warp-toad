988a0709277b0104a34e3c77b7c2ac4e5d6e56e7
jossduff <jduff360@gmail.com>
2025-04-30 14:39:58 -0400

    changed naming and impl of GigaRootBridge.sol

--- backend/contracts/evm/GigaRootBridge.sol
+++ backend/contracts/evm/GigaRootBridge.sol
@@ -1,105 +1,153 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.29;
-import "./IRootBridge.sol";
+import "./IL1RootBridgeAdapter.sol";
 import {PoseidonT3} from "poseidon-solidity/PoseidonT3.sol";
+import {LazyIMT, LazyIMTData} from "@zk-kit/lazy-imt.sol/LazyIMT.sol";
 
 contract GigaRootBridge {
-	
-	event constructedNewGigaRoot(bytes32 newGigaRoot);
-	event receivedNewL2Root(bytes32 newL2Root, address rootBridge);
+    event constructedNewGigaRoot(uint256 indexed newGigaRoot);
 
+    event receivedNewL2Root(
+        uint256 indexed newL2Root,
+        // l2 block number that this l2 root came from
+        uint32 indexed l2BlockNumber,
+        address indexed rootBridgeAdapter
+    );
+
+    LazyIMTData public rootTreeData; // does this need to be public?
     address public wrapperToken;
-    IRootBridge[] public rootBridges;
-	// mapping of the rootBridge addr to their root's position in the the l2Roots array
-    mapping(address => uint8) public rootBridgeL2RootsIndex;
-	bytes32[] public l2Roots;
-    bytes32 public gigaRoot;
-	uint8 _maxTreeDepth;
+    IL1RootBridgeAdapter[] public rootBridgeAdapters;
+    // mapping of the rootBridgeAdapter addr to their L2 root's position in all
+    // the arrays used (rootTreeData, gigaRootL2BlockNumbers, and rootBridgeAdapters)
+    mapping(address => uint40) public l2LeafIndexes;
+    // mapping gigaRoot => block number of the L2 block each L2 root (leaf) came from
+    // The length of the array is always rootBridgeAdapters.length
+    mapping(uint256 => uint32[]) public gigaRootL2BlockNumbers;
+    uint8 public maxTreeDepth;
+    uint256 public gigaRoot;
 
     /**
      * @notice Initialize the root bridge
-     * @param _bridgeAddresses - the L1 contracts that can receive roots from corresponding L2s
+     * @param _bridgeAdapterAddresses - the L1 contracts that can receive roots from corresponding L2s
      */
-    constructor(address[] memory _bridgeAddresses, uint8 _maxTreeDepth) {
-        for (uint256 i = 0; i < _bridgeAddresses.length; i++) {
-			address thisBridgeAddress = _bridgeAddresses[i];
+    constructor(address[] memory _bridgeAdapterAddresses, uint8 _maxTreeDepth) {
+        maxTreeDepth = _maxTreeDepth;
+        // initialize rootTreeData with 0s
+        LazyIMT.init(rootTreeData, _maxTreeDepth);
 
-			// add to list of rootBridges 
-            rootBridges.push(IRootBridge(thisBridgeAddress));	
+        // for each L1RootBridgeAdapter...
+        for (uint40 i = 0; i < _bridgeAdapterAddresses.length; i++) {
+            address thisBridgeAdapterAddress = _bridgeAdapterAddresses[i];
 
-			// note the index of this root bridge in the array l2Roots
-            rootBridgeL2RootsIndex[thisBridgeAddress] = i;
+            // add to list of rootBridges
+            rootBridgeAdapters.push(
+                IL1RootBridgeAdapter(thisBridgeAdapterAddress)
+            );
 
-			// initialize the root as 0
-			l2Roots[i] = 0;
+            // note the index of this root bridge.  This index will be used to in
+            // rootTreeData, gigaRootL2BlockNumbers, and rootBridgeAdapters to keep a
+            // "state" for each L2 which WarpToad is deployed on
+            l2LeafIndexes[thisBridgeAdapterAddress] = i;
         }
 
-		maxTreeDepth = _maxTreeDepth;
+        // initialize a list of 0 elements of length _bridgeAdapterAddresses.length
+        uint256 numberOfL2s = _bridgeAdapterAddresses.length;
+        uint32[] memory initialL2BlockNumbers = new uint32[](numberOfL2s);
 
-		// gigaRoot starts as 0
-        gigaRoot = 0;
+        // and set the initial root to this list
+        gigaRootL2BlockNumbers[0] = initialL2BlockNumbers;
     }
 
     /**
      * @notice updates the current gigaRoot by querying for updates from the supplied
      * list of L2 bridges.  You can pull updates from a subset of L2s for gas saving (don't have to update to all l2s).
-     * Calls corresponding contracts to update the GigaRoots on the L2.
      */
-    function updateRoot(address[] memory _bridgeAddresses) external {
-		// change the necessary roots in l2Roots array
-        for (uint256 i = 0; i < _bridgeAddresses.length; i++) {
-			address thisBridgeAddress = _bridgeAddresses[i];
+    function updateRoot(address[] memory _bridgeAdapterAddresses) external {
+        require(
+            _bridgeAdapterAddresses.length >= rootBridgeAdapters.length,
+            "Passed in too many bridgeAdapterAddresseses"
+        );
 
-			// get the index of this bridge's root in the l2Roots array
-			uint8 indexInL2Roots = rootBridgeL2RootsIndex[thisBridgeAddress];
+        // get old array of l2BlockNumbers at the previous gigaRoot and overwrite it with
+        // new block numbers of the L2s were updating
+        uint32[] memory updatedL2BlockNumbers = gigaRootL2BlockNumbers[
+            gigaRoot
+        ];
 
+        // for each l2BridgeAdapter
+        for (uint40 i = 0; i < _bridgeAdapterAddresses.length; i++) {
+            address thisBridgeAdapterAddress = _bridgeAdapterAddresses[i];
+
+            // get the index of this l2 (used in updatedL2BlockNumbers and rootTreeData)
+            uint40 l2LeafIndex = l2LeafIndexes[thisBridgeAdapterAddress];
+
+            // make sure this bridgeAdapterAddress was initialized
             require(
-				indexInL2Roots > 0,
-                "address is not a registered root bridge address"
+                l2LeafIndex > 0,
+                "Address is not a registered root bridge address"
             );
 
-            IRootBridge l2RootBridge = IRootBridge(thisBridgeAddress);
+            // create IL1RootBridgeAdapter interface
+            IL1RootBridgeAdapter rootBridgeAdapter = IL1RootBridgeAdapter(
+                thisBridgeAdapterAddress
+            );
 
-			// get the most recent l2 root from this bridge
-            bytes32 newL2Root = l2RootBridge.getMostRecentRoot();
-			emit receivedNewL2Root(newL2Root, thisBridgeAddress);
+            // get the most recent l2 root and the l2 block number it came from from this bridge
+            (uint256 newL2Root, uint32 l2BlockNumber) = rootBridgeAdapter
+                .getMostRecentRootAndL2Block();
 
-			// update the root in the corresponding index in the array
-			l2Roots[indexInL2Roots] = newL2Root;	
+            emit receivedNewL2Root(
+                newL2Root,
+                l2BlockNumber,
+                thisBridgeAdapterAddress
+            );
+
+            // update the root in the corresponding index in the merkle tree
+            // TODO: this is pretty expensive.  Optimize batch updates
+            LazyIMT.update(rootTreeData, newL2Root, l2LeafIndex);
+
+            // update the list of L2 block numbers for the L2 whos root we just got
+            updatedL2BlockNumbers[l2LeafIndex] = l2BlockNumber;
         }
 
-		bytes32 newGigaRoot = constructGigaRoot(l2Roots);
+        // compute new giga root
+        uint256 newGigaRoot = LazyIMT.root(rootTreeData);
 
-		// update all L2s with the new gigaRoot
-		for (uint256 i = 0; i < rootBridges.length; i++) {
-			IRootBridge thisRootBridge = rootBridges[i];
+        // set the updated list of L2Block numbers that this gigaRoot is updated to
+        gigaRootL2BlockNumbers[newGigaRoot] = updatedL2BlockNumbers;
 
-			// send the new gigaRoot to the L2's bridge
-			thisRootBridge.sendGigaRootToL2(newGigaRoot);
-		}
+        emit constructedNewGigaRoot(newGigaRoot);
+
+        // set new gigaRoot in contract
+        gigaRoot = newGigaRoot;
     }
 
-	// gas optimization potential here for when we update a subset of roots
-	function constructGigaRoot(bytes32[] _l2Roots) pure internal returns bytes32 {
+    // Made this a second function because the addresses that want the gigaRoot
+    // might be different from the addresses that are updating the root.
+    // Sends the most recent gigaRoot to an array of rootBridgeAdapters
+    function sendRoot(address[] memory _bridgeAdapterAddresses) external {
+        require(
+            _bridgeAdapterAddresses.length >= rootBridgeAdapters.length,
+            "Passed in too many bridgeAdapterAddresseses"
+        );
 
-		// Assumes arity of 2 (2 children per node)
-		// TODO: base case of 1 root?
-		bytes32[] hashes = _l2Roots;
-		for (uint256 d = maxTreeDepth; d > 1; d--) {
-			bytes32[] newHashes;	
-			for (uint256 i = 1; i < hashes.length; i = i + 2) {
-				bytes_32 left = _l2Roots[i-1];
-				bytes_32 right = _l2Roots[i]
+        for (uint256 i = 0; i < _bridgeAdapterAddresses.length; i++) {
+            address thisBridgeAdapterAddress = _bridgeAdapterAddresses[i];
 
-				bytes_32 hash = PoseidonT3.hash([left, right]);
-				newHashes.push(hash);
-			}	
-			hashes = newHashes;
-		}
-		
-		emit constructedNewGigaRoot(newGigaRoot);
+            // use l2LeafIndexes mapping to verify that this rootBridgeAdapterAddress was initialized
+            require(
+                l2LeafIndexes[thisBridgeAdapterAddress] > 0,
+                "Address is not a registered root bridge adapter address"
+            );
 
-		newGigaRoot;
-	}
+            IL1RootBridgeAdapter rootBridgeAdapter = IL1RootBridgeAdapter(
+                thisBridgeAdapterAddress
+            );
+
+            // send the most recent gigaRoot to this L1RootBridgeAdapter address, to later be bridged over
+            // and consumed by the corresponding L2RootBridgeAdapter on the L2
+            rootBridgeAdapter.sendGigaRootToAdapter(gigaRoot);
+        }
+    }
 }
--- backend/contracts/evm/L1AztecRootBridgeAdapter.sol
+++ backend/contracts/evm/L1AztecRootBridgeAdapter.sol
@@ -2,7 +2,8 @@
 
 pragma solidity 0.8.29;
 
-import "./IRootBridge.sol";
+import "./IL1RootBridgeAdapter.sol";
+
 // TODO: remove
 // import "hardhat/console.sol";
 
@@ -19,13 +20,18 @@ import {Hash} from "./aztec-interfaces/crypto/Hash.sol";
 // TODO: make it IRootBridge
 //is IRootBridge
 contract AztecRootBridge {
+    // gigaRoot is emitted as a bytes32 here because thats how it's recovered on the
+    // aztec L2 side of this rootBridgeAdapter.  Key and index are also used to
+    // retrieve this newGigaRoot on aztec
     event newGigaRootSentToL2(bytes32 newGigaRoot, bytes32 key, uint256 index);
-    event receivedNewL2Root(bytes32 newL2Root);
+    event receivedNewL2Root(uint256 newL2Root, uint32 l2Block);
 
     IRegistry public registry;
     bytes32 public l2Bridge;
     uint32 public aztecChainId;
-    bytes32 public mostRecentL2Root;
+    uint256 public mostRecentL2Root;
+    // the L2 block that the most recent L2 root came from
+    uint32 public mostRecentL2RootBlockNumber;
 
     IRollup public rollup;
     IOutbox public outbox;
@@ -46,14 +52,14 @@ contract AztecRootBridge {
         outbox = rollup.getOutbox();
         inbox = rollup.getInbox();
         rollupVersion = rollup.getVersion();
-        mostRecentL2Root = bytes32(0);
+        mostRecentL2Root = 0;
     }
 
     /**
      * @notice adds an L2 message which can only be consumed publicly on Aztec
      * @param _newGigaRoot - The new gigaRoot to send to L2 as a message
      */
-    function sendGigaRootToL2(bytes32 _newGigaRoot) external {
+    function sendGigaRootToL2(uint256 _newGigaRoot) external {
         // l2Bridge is the Aztec address of the contract that will be retrieving the
         // message on the L2
         DataStructures.L2Actor memory actor = DataStructures.L2Actor(
@@ -65,7 +71,7 @@ contract AztecRootBridge {
         // so it recommends you hash it and verify the hash on the L2 when retrieving the message.
         // Luckily for us, the GigaRoot is the size of a Field so we don't have to hash it
         // and can directly retrieve it on the L2.
-        bytes32 contentHash = _newGigaRoot;
+        bytes32 contentHash = bytes32(_newGigaRoot);
 
         // `secret` is used to make the consumption of a message on the L2 private.
         // we don't care about keeping message consumption private at all so to
@@ -84,7 +90,11 @@ contract AztecRootBridge {
         emit newGigaRootSentToL2(contentHash, key, index);
     }
 
-    function getMostRecentRoot() external returns (bytes32) {
+    function getMostRecentRoot() external view returns (uint256) {
+        require(
+            mostRecentL2Root > 0,
+            "An L2 root hasn't yet been bridged to this contract. refreshRoot must be called."
+        );
         return mostRecentL2Root;
     }
 
@@ -99,10 +109,11 @@ contract AztecRootBridge {
      */
     function refreshRoot(
         bytes32 _newL2Root,
-        uint256 _l2BlockNumber,
+        uint32 _l2BlockNumber,
         uint256 _leafIndex,
         bytes32[] calldata _path
-    ) external returns (bytes32) {
+    ) external returns (uint256) {
+        // TODO: new data structure to extract
         DataStructures.L2ToL1Msg memory message = DataStructures.L2ToL1Msg({
             sender: DataStructures.L2Actor(l2Bridge, rollupVersion),
             recipient: DataStructures.L1Actor(address(this), block.chainid),
@@ -111,10 +122,11 @@ contract AztecRootBridge {
 
         outbox.consume(message, _l2BlockNumber, _leafIndex, _path);
 
-        mostRecentL2Root = _newL2Root;
+        // convert from bytes32 to uint256
+        uint256 newL2RootCast = uint256(_newL2Root);
 
-        emit receivedNewL2Root(_newL2Root);
+        emit receivedNewL2Root(newL2RootCast, _l2BlockNumber);
 
-        _newL2Root;
+        newL2RootCast;
     }
 }
