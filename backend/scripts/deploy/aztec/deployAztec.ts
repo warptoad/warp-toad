e0829984dec0ba9b251716be2f4c2e910b56b525
nodestarQ <node@perfectqstudios.com>
2025-11-06 11:24:35 +0100

    added utils functions for aztec

--- backend/scripts/deploy/aztec/deployAztec.ts
+++ backend/scripts/deploy/aztec/deployAztec.ts
@@ -1,12 +1,6 @@
 
 import { ethers } from "ethers";
 import * as hre from "hardhat";
-import { Wallet as AztecWallet, BaseWallet } from "@aztec/aztec.js/wallet"
-import { createStore } from "@aztec/kv-store/lmdb";
-import { AztecNode, createAztecNodeClient } from "@aztec/aztec.js/node";
-import { createPXE, getPXEConfig, PXE } from "@aztec/pxe/server";
-import { getInitialTestAccountsData, InitialAccountData } from '@aztec/accounts/testing';
-import { TestWallet } from "@aztec/test-wallet/server";
 
 
 //@ts-ignore
@@ -15,81 +9,10 @@ import { checkFileExists, getAztecDeployedAddressesFilePath, getAztecDeployedAdd
 import fs from "fs/promises";
 import { deployAztecWarpToad } from "./aztecToadWarp";
 import { deployL2AztecBridgeAdapter } from "./L2AztecBridgeAdapter";
+import { getAztecTestAccounts, getEnvArgs, initNodeClient, initPXE } from "../utils/aztecUtils";
 
-const { nativeTokenAddress, PXE_URL, privateKey } = getEnvArgs()
+const { nativeTokenAddress } = getEnvArgs()
 
-function getEnvArgs() {
-    if (!Boolean(process.env.NATIVE_TOKEN_ADDRESS)) {
-        throw new Error("NATIVE_TOKEN_ADDRESS not set. do: L1_AZTEC_ADAPTER_ADDRESS=0xTheAdapterAddress NATIVE_TOKEN_ADDRESS=0xUrTokenAddress yarn workspace @warp-toad/backend hardhat run scripts/deploy/deployAztec.ts --network aztecSandbox")
-    } else if (!ethers.isAddress(process.env.NATIVE_TOKEN_ADDRESS)) {
-        throw new Error(`the value: ${process.env.NATIVE_TOKEN_ADDRESS} is not a valid address. Set NATIVE_TOKEN_ADDRESS= to a valid address`)
-    }
-
-    if (!Boolean(process.env.PXE_URL)) {
-        throw new Error("PXE_URL not set. do PXE_URL=http://UR.PXE NATIVE_TOKEN_ADDRESS=0xurTokenAddress yarn workspace @warp-toad/backend hardhat run scripts/deploy/deployAztec.ts  --network aztecSandbox")
-    }
-
-
-    const nativeTokenAddress = ethers.getAddress(process.env.NATIVE_TOKEN_ADDRESS as string);
-    const PXE_URL = process.env.PXE_URL as string
-    return { nativeTokenAddress, PXE_URL, privateKey: process.env.PRIVATE_KEY }
-}
-
-async function initNodeClient(): Promise<AztecNode> {
-    try {
-        console.log("creating Aztec Node Client...");
-        const node = createAztecNodeClient(PXE_URL);
-        const nodeInfo = await node.getNodeInfo();
-        console.log("Connected to sandbox version:", nodeInfo.nodeVersion);
-        console.log("Chain ID:", nodeInfo.l1ChainId);
-        return node;
-
-    } catch (error) {
-        console.log("failed to create Aztec Node Client: ", error);
-        throw error;
-    }
-}
-
-async function initPXE(node: AztecNode): Promise<PXE> {
-    try {
-        const l1Contracts = await node.getL1ContractAddresses();
-        console.log("creating PXE client");
-        const config = getPXEConfig();
-        const fullConfig = { ...config, l1Contracts };
-        fullConfig.proverEnabled = false; // you'll want to set this to "true" once you're ready to connect to the testnet
-
-        const store = await createStore("pxe", {
-            dataDirectory: "store",
-            dataStoreMapSizeKb: 1e6,
-        });
-        const pxe = await createPXE(node, fullConfig, { store });
-        return pxe
-
-    } catch (error) {
-        console.log("failed to create Aztec PXE: ", error);
-        throw error;
-    }
-}
-
-async function setupWallet(node: AztecNode): Promise<TestWallet> {
-    const wallet = await TestWallet.create(node);
-    return wallet;
-}
-
-async function getAztecTestAccounts(chainId: bigint) {
-    const wallet = await setupWallet(await initNodeClient());
-    const testAccountData = (await getInitialTestAccountsData())[0];
-
-    if (chainId == 31337n) {
-        console.warn("assuming ur on sandbox since chainId is 31337")
-        await wallet.createSchnorrAccount(testAccountData.secret, testAccountData.salt);
-        return wallet
-    } else {
-        console.warn("assuming ur on testnet since chainId is NOT 31337")
-        await wallet.createSchnorrAccount(testAccountData.secret, testAccountData.salt);
-        return wallet
-    }
-}
 
 async function main() {
 
@@ -111,15 +34,13 @@ async function main() {
             return 0
         }
     }
-    
-    const pxe = await initPXE((await initNodeClient()));
 
-    const wallet = await getAztecTestAccounts(chainId) //(await getAztecWallet(PXE, privateKey as string, "https://aztec-alpha-testnet-fullnode.zkv.xyz", chainId))//wallets[0]
+    const wallet = await getAztecTestAccounts(chainId)
 
     //------deploy-------------
     const { AztecWarpToad } = await deployAztecWarpToad(nativeToken, wallet, undefined)
     console.log({ AztecWarpToad: AztecWarpToad.address })
-    
+
     const { L2AztecBridgeAdapter } = await deployL2AztecBridgeAdapter(L1AztecAdapterAddress, wallet, undefined)
     console.log({ L2AztecBridgeAdapter: L2AztecBridgeAdapter.address })
     const deployments = { AztecWarpToad: AztecWarpToad.address, L2AztecBridgeAdapter: L2AztecBridgeAdapter.address }
@@ -131,7 +52,7 @@ async function main() {
         AztecWarpToad:              ${AztecWarpToad.address}
         L2AztecBridgeAdapter:       ${L2AztecBridgeAdapter.address}
     `)
-    
+
 
 }
 
---
+++ backend/scripts/deploy/utils/aztecUtils.ts
@@ -0,0 +1,91 @@
+
+import { ethers } from "ethers";
+import { Wallet as AztecWallet, BaseWallet } from "@aztec/aztec.js/wallet"
+import { createStore } from "@aztec/kv-store/lmdb";
+import { AztecNode, createAztecNodeClient } from "@aztec/aztec.js/node";
+import { createPXE, getPXEConfig, PXE } from "@aztec/pxe/server";
+import { getInitialTestAccountsData, InitialAccountData } from '@aztec/accounts/testing';
+import { TestWallet } from "@aztec/test-wallet/server";
+
+const PXE_URL = getPxeUrl()
+
+export function getPxeUrl() {
+    if (!Boolean(process.env.PXE_URL)) {
+        throw new Error("PXE_URL not set. do PXE_URL=http://UR.PXE NATIVE_TOKEN_ADDRESS=0xurTokenAddress yarn workspace @warp-toad/backend hardhat run scripts/deploy/deployAztec.ts  --network aztecSandbox")
+    }
+    const PXE_URL = process.env.PXE_URL as string
+    return PXE_URL
+}
+
+export function getEnvArgs() {
+    if (!Boolean(process.env.NATIVE_TOKEN_ADDRESS)) {
+        throw new Error("NATIVE_TOKEN_ADDRESS not set. do: L1_AZTEC_ADAPTER_ADDRESS=0xTheAdapterAddress NATIVE_TOKEN_ADDRESS=0xUrTokenAddress yarn workspace @warp-toad/backend hardhat run scripts/deploy/deployAztec.ts --network aztecSandbox")
+    } else if (!ethers.isAddress(process.env.NATIVE_TOKEN_ADDRESS)) {
+        throw new Error(`the value: ${process.env.NATIVE_TOKEN_ADDRESS} is not a valid address. Set NATIVE_TOKEN_ADDRESS= to a valid address`)
+    }
+
+    if (!Boolean(process.env.PXE_URL)) {
+        throw new Error("PXE_URL not set. do PXE_URL=http://UR.PXE NATIVE_TOKEN_ADDRESS=0xurTokenAddress yarn workspace @warp-toad/backend hardhat run scripts/deploy/deployAztec.ts  --network aztecSandbox")
+    }
+
+
+    const nativeTokenAddress = ethers.getAddress(process.env.NATIVE_TOKEN_ADDRESS as string);
+    const PXE_URL = process.env.PXE_URL as string
+    return { nativeTokenAddress, PXE_URL, privateKey: process.env.PRIVATE_KEY }
+}
+
+export async function initNodeClient(): Promise<AztecNode> {
+    try {
+        console.log("creating Aztec Node Client...");
+        const node = createAztecNodeClient(PXE_URL);
+        const nodeInfo = await node.getNodeInfo();
+        console.log("Connected to sandbox version:", nodeInfo.nodeVersion);
+        console.log("Chain ID:", nodeInfo.l1ChainId);
+        return node;
+
+    } catch (error) {
+        console.log("failed to create Aztec Node Client: ", error);
+        throw error;
+    }
+}
+
+export async function initPXE(node: AztecNode): Promise<PXE> {
+    try {
+        const l1Contracts = await node.getL1ContractAddresses();
+        console.log("creating PXE client");
+        const config = getPXEConfig();
+        const fullConfig = { ...config, l1Contracts };
+        fullConfig.proverEnabled = false; // you'll want to set this to "true" once you're ready to connect to the testnet
+
+        const store = await createStore("pxe", {
+            dataDirectory: "store",
+            dataStoreMapSizeKb: 1e6,
+        });
+        const pxe = await createPXE(node, fullConfig, { store });
+        return pxe
+
+    } catch (error) {
+        console.log("failed to create Aztec PXE: ", error);
+        throw error;
+    }
+}
+
+export async function setupWallet(node: AztecNode): Promise<TestWallet> {
+    const wallet = await TestWallet.create(node);
+    return wallet;
+}
+
+export async function getAztecTestAccounts(chainId: bigint) {
+    const wallet = await setupWallet(await initNodeClient());
+    const testAccountData = (await getInitialTestAccountsData())[0];
+
+    if (chainId == 31337n) {
+        console.warn("assuming ur on sandbox since chainId is 31337")
+        await wallet.createSchnorrAccount(testAccountData.secret, testAccountData.salt);
+        return wallet
+    } else {
+        console.warn("assuming ur on testnet since chainId is NOT 31337")
+        await wallet.createSchnorrAccount(testAccountData.secret, testAccountData.salt);
+        return wallet
+    }
+}
